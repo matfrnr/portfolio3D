3e6b0ba7d77509839cd8fe11648f268d5e8159db
Matheo <mat@gmail>
2025-03-21 14:09:10 +0100

    Css

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -175,7 +175,7 @@ const Works = () => {
         </>
       )}
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-10 flex flex-wrap gap-0 md:gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}

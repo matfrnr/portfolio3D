3bc2ba3dd59ff89c39fed243cf9fa61f4de92f0f
Mathéo <97743381+matfrnr@users.noreply.github.com>
2026-06-28 16:21:37 +0200

    Delete .vite/deps directory

--- .vite/deps/react-dom_client.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_client
-} from "./chunk-AETEVXWA.js";
-import "./chunk-EBHHJRO4.js";
-import "./chunk-CJK34UIW.js";
-import "./chunk-W24JOBID.js";
-import "./chunk-EWTE5DHJ.js";
-export default require_client();
